b75955b19bdb0cc58ff2e6bde74df01055f2bad1
lisamiloche <124432385+lisamiloche@users.noreply.github.com>
2023-12-12 18:28:18 +0100

    life du player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,14 +9,21 @@ public class Player : MonoBehaviour
     public GameObject spellPrefab;
     public GameObject Enemy;
     public GameObject player;
+    public int playerLife;
 
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.name == "Enemy")
         {
-            //Debug.Log(col.gameObject.name);
-            Destroy(player);
+            playerLife--;
+            Debug.Log(playerLife);
+            //faire un timer pour que ça ne tue pas d'un seul coup
+            if (playerLife <= 0 )
+            {
+                Destroy(player); //animation de mort
+            }
+
         }
     }
 
