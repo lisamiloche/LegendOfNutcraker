cfbd1c7936c3a7f965d2ef863e04afb65c1e4408
lisamiloche <124432385+lisamiloche@users.noreply.github.com>
2023-12-12 14:16:37 +0100

    collision projectile/ennemi

--- Assets/Scripts/Enemies.cs
+++ Assets/Scripts/Enemies.cs
@@ -4,26 +4,12 @@ using UnityEngine;
 
 public class Enemies : MonoBehaviour
 {
-    public float speed = 3f;
-    public float distance = 7f;
-
-    private Transform player;
-
-    void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-
-    }
+    public float speed;
+  
 
     void Update()
     {
         transform.Translate(Vector2.left * speed * Time.deltaTime);
-        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
-
-        if (distanceToPlayer < distance)
-        {
-            player.GetComponent<Player>().LaunchSpell(transform.position);
-        }
     }
 
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,39 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
 
-    public float distance = 5f;
-    public GameObject spellPrefab;
-
-    void Update()
-    {
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, distance);
+   
 
 
-        foreach (Collider2D collider in colliders)
-        {
+   
 
-            if (collider.CompareTag("Enemy"))
-            {
-                LaunchSpell(collider.transform.position);
-                break;
-            }
-
-        }
-
-
-    }
-
-    public void LaunchSpell(Vector3 targetPosition)
-    {
-        GameObject spellInstance = Instantiate(spellPrefab, transform.position, Quaternion.identity);
-        Vector3 direction = (targetPosition - transform.position).normalized;
-        spellInstance.GetComponent<Spell>().SetDirection(direction);
-
-    }
 
 }
